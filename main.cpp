e50bf7f005cba5b1c73b1b3e4a388dec7f28595a
Irokanade <michaeljs.leong@gmail.com>
2026-04-22 16:03:02 +0800

    feat: update driver code

--- main.cpp
+++ main.cpp
@@ -1,18 +1,21 @@
 #include "cpu.h"
+#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 
-constexpr int N = 4096;
-static uint8_t matrix[N][N];
+constexpr int ROWS = 4096;
+constexpr int COLS = 4096;
+constexpr uint64_t MEMORY_SIZE = static_cast<uint64_t>(ROWS) * COLS;
+static uint8_t memory[MEMORY_SIZE];
 
 static void row_major_order(CPU *cpu)
 {
     uint8_t tmp;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cpu_read(cpu, 0, reinterpret_cast<uint64_t>(&matrix[i][j]), &tmp,
-                     sizeof(uint8_t));
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            uint64_t addr = static_cast<uint64_t>(i * COLS + j);
+            cpu_read(cpu, 0, addr, &tmp, sizeof(uint8_t), memory);
         }
     }
 }
@@ -20,10 +23,10 @@ static void row_major_order(CPU *cpu)
 static void col_major_order(CPU *cpu)
 {
     uint8_t tmp;
-    for (int j = 0; j < N; j++) {
-        for (int i = 0; i < N; i++) {
-            cpu_read(cpu, 0, reinterpret_cast<uint64_t>(&matrix[i][j]), &tmp,
-                     sizeof(uint8_t));
+    for (int j = 0; j < COLS; j++) {
+        for (int i = 0; i < ROWS; i++) {
+            uint64_t addr = static_cast<uint64_t>(i * COLS + j);
+            cpu_read(cpu, 0, addr, &tmp, sizeof(uint8_t), memory);
         }
     }
 }
@@ -37,15 +40,17 @@ int main()
     }
 
     row_major_order(cpu);
-    std::printf("row-major: L1D misses: %llu L2 misses: %llu\n",
+    std::printf("row-major: L1D misses: %llu L2 misses: %llu L3 misses: %llu\n",
                 cpu->cores[0].perf_counters.l1d_misses,
-                cpu->cores[0].perf_counters.l2_misses);
+                cpu->cores[0].perf_counters.l2_misses,
+                cpu->cores[0].perf_counters.l3_misses);
 
     std::memset(cpu, 0, sizeof(CPU));
     col_major_order(cpu);
-    std::printf("col-major: L1D misses: %llu L2 misses: %llu\n",
+    std::printf("col-major: L1D misses: %llu L2 misses: %llu L3 misses: %llu\n",
                 cpu->cores[0].perf_counters.l1d_misses,
-                cpu->cores[0].perf_counters.l2_misses);
+                cpu->cores[0].perf_counters.l2_misses,
+                cpu->cores[0].perf_counters.l3_misses);
 
     std::free(cpu);
     return 0;
